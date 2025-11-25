e886d706b461dc2e0a2a721f346d30a6b16d5f3b
Kevin Joe Jaisingh <kevinjoejaisingh@users.noreply.github.com>
2025-11-24 22:33:49 -0800

    Switch to BrowserRouter for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'  // Change back to BrowserRouter
 import Header from './components/Header'
 import Home from './pages/Home'
 import ServicesPage from './pages/ServicesPage'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import './Hero.css'
 import BeforeAfter from './BeforeAfter'
+import detailImg1 from '../assets/images/_DSC2036.jpg'
+import detailImg2 from '../assets/images/_DSC2336.jpg'
 
 function Hero() {
   return (
@@ -9,6 +11,12 @@ function Hero() {
         <p>Where craftsmanship meets excellence</p>
       </div>
       <BeforeAfter />
+      
+      <div className="detail-gallery">
+        <img src={detailImg1} alt="Detail work close-up" />
+        <img src={detailImg2} alt="Detail work close-up" />
+      </div>
+
       <div className="scroll-indicator">
         <div>↓</div>
       </div>
