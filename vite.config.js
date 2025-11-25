abf3b1c7c5848e687af6c4405971a1eeb05abd0e
Kevin Joe Jaisingh <kevinjoejaisingh@users.noreply.github.com>
2025-11-24 22:57:23 -0800

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Freedle-Cleans/',
+  base: '/' // Change from '/Freedle-Cleans/' to '/'
 })
